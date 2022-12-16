ce48718236b08e0bb71327960ff1e0208d49f67b
mohammadlam <md.nurul.islamlam2000@gmail.com>
2022-12-16 19:56:16 +0600

    added validation

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -5,21 +5,28 @@ import { addTask } from '../redux/tasks/actions'
 const AddTask: React.FC = () => {
   // state for store task name
   const [name, setName] = useState('')
+  // this state will handle error message
+  const [error, setError] = useState('')
   const dispatch = useDispatch()
 
   const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value)
+    setError('')
   }
 
   // onSubmit function
   const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    dispatch(addTask(name))
+    if (name) {
+      dispatch(addTask(name))
+    } else {
+      setError('Please type your task')
+    }
     setName('')
   }
 
   return (
-    <form onSubmit={submitHandler} className='flex gap-3'>
+    <form onSubmit={submitHandler} className='flex gap-3 relative'>
       <input
         className='border-2 w-[400px] h-10 px-5 placeholder:italic placeholder:text-gray-500 outline-none rounded-lg'
         placeholder='Type your task name here'
@@ -27,6 +34,7 @@ const AddTask: React.FC = () => {
         value={name}
         onChange={onChangeHandler}
       />
+      {error && <p className='text-xs font-medium text-red-500 absolute -bottom-6'>{error}</p>}
       <button type='submit' className='h-10 bg-blue-500 text-white px-5 rounded-lg'>
         Add
       </button>
